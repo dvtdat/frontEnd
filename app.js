0f09182a9f8e8c353ea5c3ff23ed6163d5886020
dvtdat <tiendat0417@gmail.com>
2021-08-19 23:23:16 +0700

    js implementation

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-const s = "Hello world, computer, gay, les, haha, hme, chmua hme";
+const btn = document.querySelector('.btn');
 
-console.log(s.split(', '));
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector('#myForm').style.background = '#ccc';
+
+    document.querySelector('body').classList.add('bg-dark')
+});
